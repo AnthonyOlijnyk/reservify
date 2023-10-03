106b51c17de159f69869e503eb9af5519a33600c
Harkirat <Harkirat.s.gill@ryerson.ca>
2023-10-03 18:38:03 -0400

    Renamed reservifyApp to UserApp, added RestaurantApp

---
+++ backend/UserApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    username = models.CharField(max_length=255, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.name
