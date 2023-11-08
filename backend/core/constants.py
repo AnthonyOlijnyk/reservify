63143a4c8d92f9cae5fd3048e9198c506358baf6
AnthonyOlijnyk <anthony.olijnykabo@gmail.com>
2023-11-08 02:21:22 -0500

    replace constants

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -1,2 +1,10 @@
-AUTHORIZATION_ERROR_NOT_LOGGED_IN = "Unauthorized action. Please login before trying to make a reservation"
-AUTHORIZATION_ERROR_SESSION_EXPIRED = "Your session has expired. Please log in again."
+AUTHORIZATION_ERROR_NOT_LOGGED_IN = {
+    'authorization': [
+        'Unauthorized action. Please login before trying to make a reservation.'
+    ]
+}
+AUTHORIZATION_ERROR_SESSION_EXPIRED = {
+    'authorization': [
+        'Your session has expired. Please log in again.'
+    ]
+}
