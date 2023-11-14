71a8bc90113001fcd7ebdba8417382ed37e2c85e
priscillto <priscillaton@gmail.com>
2023-11-14 01:45:43 -0500

    Update RestTemplate.js Restaurant listings
    A little styling for restaurant listings

--- frontend/src/components/RestTemplate.js
+++ frontend/src/components/RestTemplate.js
@@ -1,72 +1,96 @@
-import { useMemo } from "react";
-import { useCallback } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import "./RestTemplate.css";
+.fomofuku,
+.latest-property-3-child {
+  position: absolute;
+  top: -10px;
+  left: -9.5px;
+  width: 299px;
+  height: 360px;
+}
 
-const RestTemplate = ({
-  restaurant,
-  restaurantName,
-  dimensionCode,
-  imageDimensionCode,
-  culturalOrigin,
-  address,
-  propLeft,
-  propHeight,
-  propBottom,
-  propWidth,
-}) => {
 
-  const navigate = useNavigate();
-
-  const latestProperty3Style = useMemo(() => {
-    return {
-      left: propLeft,
-    };
-  }, [propLeft]);
-
-  const fiveStarsIconStyle = useMemo(() => {
-    return {
-      height: propHeight,
-      bottom: propBottom,
-    };
-  }, [propHeight, propBottom]);
-
-  const japaneseStyle = useMemo(() => {
-    return {
-      width: propWidth,
-    };
-  }, [propWidth]);
-
-  const onReserveBtnClick = useCallback((restaurant) => {
-    console.log(restaurant);
-    navigate(`/reservepage/${restaurantName}`, { state: { restaurant } });
-  }, [navigate]);
-
-  return (
-    <div className="latest-property-3" style={latestProperty3Style}>
-      <img className="latest-property-3-child" alt="" src="/rectangle-72.svg" />
-      <b className="fomofuku">{restaurantName}</b>
-      <img
-        className="five-stars-icon2"
-        alt=""
-        src={dimensionCode}
-        style={fiveStarsIconStyle}
-      />
-      <img className="latest-property-3-item" alt="" src={imageDimensionCode} />
-      <div className="japanese3" style={japaneseStyle}>
-        {culturalOrigin}
-      </div>
-      <img
-        className="latest-property-3-inner"
-        alt=""
-        src="/rectangle-8-stroke.svg"
-      />
-      <img className="latest-property-3-inner" alt="" src="/rectangle-8.svg" />
-      <img className="reserve-icon4" alt="" src="/reserve.svg" onClick={() => onReserveBtnClick(restaurant)}></img>
-      <img className="heart-icon4" alt="" src="/heart.svg" />
-      <div className="main-street-toronto">{address}</div>
-    </div>
-  );
-};
-
-export default RestTemplate;
+.fomofuku {
+  top: 186px;
+  left: 22px;
+  font-size: var(--font-size-lg);
+  display: inline-block;
+  color: var(--color-crimson);
+  width: 164px;
+  height: 17px;
+  
+  
+}
+.five-stars-icon2 {
+  position: absolute;
+  height: 4.12%;
+  width: 33.99%;
+  top: 61.3%;
+  right: 58.14%;
+  bottom: 30.88%;
+  left: 7%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.latest-property-3-item {
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: var(--br-5xs) var(--br-5xs) 0 0;
+  width: 279px;
+  height: 172px;
+  object-fit: cover;
+}
+.japanese3 {
+  position: absolute;
+  top: 227.5px;
+  left: 22px;
+  display: inline-block;
+  width: 81px;
+  height: 14px;
+}
+.latest-property-3-inner {
+  position: absolute;
+  top: 294px;
+  left: 21.5px;
+  width: 107px;
+  height: 35px;
+}
+.heart-icon4,
+.reserve-icon4 {
+  cursor: pointer;
+  position: absolute;
+  top: 304.4px;
+  left: 40.2px;
+  width: 69px;
+  height: 11.8px;
+}
+.heart-icon4 {
+  height: 7.44%;
+  width: 10.23%;
+  top: 5.88%;
+  right: 7.66%;
+  bottom: 86.68%;
+  left: 82.11%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+}
+.main-street-toronto {
+  position: absolute;
+  top: 250px;
+  left: 22px;
+  display: inline-block;
+  width: 197px;
+  height: 14px;
+}
+.latest-property-3 {
+  position: absolute;
+  top: 202px;
+  left: 633.5px;
+  width: 279.5px;
+  height: 340px;
+  text-align: left;
+  font-size: var(--font-size-smi);
+  color: var(--color-black);
+  font-family: var(--font-montserrat);
+}
